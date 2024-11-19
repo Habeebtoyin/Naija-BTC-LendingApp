d2dd3e40621d1a3745c59f94d40289215acec9d8
Amaechi Chisom <amaechilegend@gmail.com>
2024-11-19 10:35:51 +0100

    center placed the get started button

--- components/DisconnectedTab.jsx
+++ components/DisconnectedTab.jsx
@@ -10,12 +10,12 @@ const DisconnectedTab = () => {
         <div className="md:text-[45px] text-[24px] text-center mx-10 font-bold my-5 max-w-[500px] "> Crypto Lending made easy, borrow with easy with <span className="text-[#854CFF]">DefiConnect</span></div>
          {/* <Image src={metamask} alt="Only-layer" className="md:w-[520px] md:h-[245px] h-[150px] w-[300px] shadow-md opacity-90 mx-4 rounded-md" />  */}
       </div>
-      <div className="flex items-center ml-10 p-4">
+      <div className="flex justify-center items-center ml-10 p-4">
         <div className="mr-5 button_border rounded-full p-3 hover:bg-gray-900 cursor-pointer px-6"> Get started</div>
-      <ConnectButton />
+        <ConnectButton />
       </div>
 
-      <div className="bg-black/60 p-4 rounded-lg w-full flex justify-around md:max-w-[1100px] text-center max-w-[400px] font-bold mx-auto md:my-5 mt-20 text-white text-[16px] ">
+      <div className="bg-black/60 p-4 rounded-lg w-full flex justify-around md:max-w-[1100px] text-center max-w-[400px] font-bold mx-auto md:my-5 mt-28 text-white text-[16px] ">
         <div className="p-6 border-r-sky-600 border-r-2">  Earn Interest % </div>
         <div className="p-6 border-r-sky-600 border-r-2">  Lending $ </div>
         <div className="p-6 border-r-sky-600 border-r-2">  Low transaction </div>
